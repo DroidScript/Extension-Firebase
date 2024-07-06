379e5abe5a163977ef9c70e6c452668f9c9d8c14
Oarabile Koore <oarabilekoore@protonmail.com>
2024-07-06 20:58:33 +0200

    version 0.0.1

--- Commander/Commands.js
+++ Commander/Commands.js
@@ -1,46 +1,77 @@
-/*
-Description: Firebase Command extension for DroidScript
-Author(s): D.Hurren, you?
-Copyright: droidscript.org
-License: Apache V2
-*/
+/**
+ * @description
+ * Commands.js File Recieves Command & Calls It By
+ * Running Node Instance Of Our node_Firebase.js
+ * File
+ * 
+ * @authors 
+ * D. Hurren, O.Koore
+ */
 
 //Make sure node sever is running.
-function Init() {
-    //Start menu server (with unique context id) on first show of 'Firebase' context menu.
-    if (!glob[ext_progId]) {
-        var dir = "/sdcard/DroidScript/Extensions/Firebase/Server";
-        ext_NodeRun(
-            dir + "/node_Firebase.js",
-            ext_progId,
-            dir + "/node_modules",
-        );
-        glob[ext_progId] = true;
-    }
+function Init () {
+    /**
+     * Start menu server (with unique context id) 
+     * on first show of 'Firebase' context menu. 
+    */
+    if( !glob[ext_progId] ) { 
+        var dir = "/sdcard/DroidScript/Extensions/Firebase/Server"
+        ext_NodeRun( dir+"/node_Firebase.js", ext_progId, dir + "/node_modules" )
+        glob[ext_progId] = true
+    } else { ; }
 }
 
-//Called when using menus or wifi ide '!firebase' commands.
-function Firebase_OnCommand(cmd) {
-    Init();
+// Called when using menus or wifi ide '!firebase' commands.
+// Issue Why Does Wifi Ide Call firebase_OnCommand instead
+// of Firebase_OnCommand
 
+function Firebase_OnCommand( cmd ) {
+    Init()
+    
     //Execute menu command in our node node_Firebase.js file.
-    ext_NodeExec(cmd.toLowerCase() + "()", ext_progId);
+    ext_NodeExec( cmd.toLowerCase()+"()", ext_progId ) 
 }
 
-//Called when 'Firebase' device context menu is selected.
+function firebase_OnCommand( cmd ) {
+    Init()
+    
+    ext_NodeExec( cmd.toLowerCase()+"()", ext_progId ) 
+}
+
+
+// Called when 'Firebase' device context menu is selected.
 function Firebase_OnMenu() {
-    Init();
-
+    Init()
+    
     //Show Firebase sub-menus.
-    dlgFirebase = app.CreateListDialog("Firebase", "Serv,Deploy");
-    dlgFirebase.Show();
-
+    dlgFirebase = app.CreateListDialog( "Firebase", "Serv,Deploy" )
+    dlgFirebase.Show()
+    
     //Handle menu selection.
-    dlgFirebase.SetOnTouch((cmd) => {
+    dlgFirebase.SetOnTouch( (cmd)=>{ 
+        
         //Show fresh debug window.
-        app.ShowDebug(true, "dialog,clear");
-
+        app.ShowDebug( true, "dialog,clear" )
+        
         //Execute menu command in our node node_Firebase.js file.
-        ext_NodeExec(cmd.toLowerCase() + "()", ext_progId);
-    });
+        ext_NodeExec( cmd.toLowerCase()+"()", ext_progId ) 
+    })
+}
+
+function firebase_OnMenu() {
+    Init()
+    
+    //Show Firebase sub-menus.
+    dlgFirebase = app.CreateListDialog( "Firebase", "Serv,Deploy" )
+    dlgFirebase.Show()
+    
+    //Handle menu selection.
+    dlgFirebase.SetOnTouch( (cmd)=>{ 
+        
+        //Show fresh debug window.
+        app.ShowDebug( true, "dialog,clear" )
+        
+        //Execute menu command in our node node_Firebase.js file.
+        ext_NodeExec( cmd.toLowerCase()+"()", ext_progId ) 
+    })
 }
--- README.md
+++ README.md
@@ -1,2 +1,74 @@
-# Extension-Firebase
-DroidScript Firebase Extension
+# DroidScript Firebase Extension
+
+The first thing that you should have is a firebase project, and a node based DroidScript project.
+
+## Firebase Set-up
+
+The next step is to get your firebase key (the config), you should find this in the project settings, copy the config and create the file `firebase.json`.
+It should be at the top level of your project, not in a subfolder.
+
+Copy the json in that area, create a file called firebase.json
+![](docImages/slide1.png)
+
+In your firebase.json code extend it to include the hosting object, the final thing should look like this :
+
+```json
+{
+  "apiKey": "Something",
+  "authDomain": "Something.com",
+  "projectId": "someId",
+  "storageBucket": "somebucket.appspot.com",
+  "messagingSenderId": "12345678",
+  "appId": "2;498u88943u894i",
+  "measurementId": "Krfhr94893",
+  "hosting": {
+    "public": "public",
+    "ignore": [
+      "firebase.json",
+      "**/.*",
+      "**/node_modules/**"
+    ],
+    "rewrites": [
+      {
+        "source": "**",
+        "destination": "/index.html"
+      }
+    ]
+  }
+}
+```
+
+The next step is to configure admin access, so move to the DroidScript folder, you should find folders like .edit, Extensions.
+
+In the DroidScript folder add a new folder called `.keys`.
+Then add a folder with the same name as your project.
+
+![Folder in DroidScript/.keys](docImages/slide2.png)
+
+In that folder add the file `firebase-key.json`, this file is not the same as the firebase.json file made earlier.
+
+![Firebase Dashboard @ service accounts](docImages/slide3.png)
+
+This file should have the service account json keys, click generate new private key, and a file will be downloaded, copy its contents and create firebase-key.json in the folder.
+
+![Folder in DroidScript/.keys/projectname](docImages/slide4.png)
+
+The last step is to create a public folder and add an index.html file, in your DroidScript project folder.
+
+## Available Commands
+
+1; !firebase deploy
+
+Deploys your project, it will log the url to visit the deployment is the files in the public folder.
+
+2; !firebase serv
+
+It serves you static files in the public folder to port 5000
+To visit type in the ip adress provided by DroidScript Wifi Ide, and change the 8088 port to 5000
+
+i.e; 
+DroidScript Ip is : http://192.168.1.181:8088/ change to : http://192.168.1.181:5000/
+
+3; !firebase list
+
+It lists all your projects.
